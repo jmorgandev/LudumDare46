81b7603bcc642deea851cb9af0cdb201d972bdd7
Jamie Morgan <jamiemorgan.dev@gmail.com>
2020-04-19 15:37:54 +0100

    It works

--- Assets/Scripts/Sprite3DRenderer.cs
+++ Assets/Scripts/Sprite3DRenderer.cs
@@ -8,17 +8,27 @@ public class Sprite3DRenderer : MonoBehaviour
 
     private int angle_step = 0;
     private SpriteRenderer sprite_renderer;
+
+    private Transform physics_object;
     void Start()
     {
         sprite_renderer = GetComponent<SpriteRenderer>();
         angle_step = 180 / frames.Count;
+
+        physics_object = transform.parent;
+        transform.SetParent(null);
     }
 
     // Update is called once per frame
     void Update()
     {
         // Access frames based on Y-angle
-        sprite_renderer.flipX = transform.rotation.y > (angle_step * frames.Count);
-        sprite_renderer.sprite = frames[(int)transform.rotation.y % angle_step];
+        sprite_renderer.flipX = Mathf.Abs(physics_object.rotation.eulerAngles.y) % 180 < 90;
+        sprite_renderer.sprite = frames[Mathf.Abs((int)(physics_object.rotation.eulerAngles.y / angle_step)) % frames.Count];
+
+        Debug.Log(sprite_renderer.flipX);
+
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+        transform.position = physics_object.position;
     }
 }

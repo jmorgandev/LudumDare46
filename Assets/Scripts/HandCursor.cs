7e05617e1951316569907f263678207e06392971
Jamie Morgan <jamiemorgan.dev@gmail.com>
2020-04-19 16:27:23 +0100

    Got some cursor action

---
+++ Assets/Scripts/HandCursor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCursor : MonoBehaviour
+{
+    public Sprite default_hand;
+    public Sprite hover_hand;
+    public Sprite closed_hand;
+
+    SpriteRenderer sprite_renderer;
+
+    void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite_renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = pos;
+
+        
+        if (Input.GetMouseButton(0))
+            sprite_renderer.sprite = hover_hand;
+        else
+            sprite_renderer.sprite = default_hand;
+    }
+}

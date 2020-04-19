e7abb978fc4678aef900cd540dd75063250ba26e
Jamie Morgan <jamiemorgan.dev@gmail.com>
2020-04-19 04:42:53 +0100

    stuff

---
+++ Assets/Scripts/Sprite3DRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite3DRenderer : MonoBehaviour
+{
+    public List<Sprite> frames;
+
+    private int angle_step = 0;
+    private SpriteRenderer sprite_renderer;
+    void Start()
+    {
+        sprite_renderer = GetComponent<SpriteRenderer>();
+        angle_step = 180 / frames.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Access frames based on Y-angle
+        sprite_renderer.flipX = transform.rotation.y > (angle_step * frames.Count);
+        sprite_renderer.sprite = frames[(int)transform.rotation.y % angle_step];
+    }
+}

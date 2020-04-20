cb1e909ee1ffffd27ee068cd9ff3385f65eba937
Jamie Morgan <jamiemorgan.dev@gmail.com>
2020-04-20 05:14:04 +0100

    some bits

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -7,6 +7,8 @@ public class Fire : MonoBehaviour
     private Animator fire_animator;
     private Animator fuel_animator;
 
+    private CircleCollider2D item_area;
+
     private int fire_health;
 
     // Start is called before the first frame update
@@ -14,10 +16,11 @@ public class Fire : MonoBehaviour
     {
         fire_animator = transform.Find("FireSprite").GetComponent<Animator>();
         fuel_animator = transform.Find("FuelSprite").GetComponent<Animator>();
-        fuel_animator.StopPlayback();
+        item_area = GetComponent<CircleCollider2D>();
 
         Debug.Log(fire_health);
-        InvokeRepeating("doHealth", 0f, 2f);
+        UpdateHealthEffects();
+        InvokeRepeating("doHealth", 2f, 2f);
     }
 
     // Update is called once per frame
@@ -26,23 +29,31 @@ public class Fire : MonoBehaviour
         
     }
 
+    private void UpdateHealthEffects()
+    {
+        if (fire_health >= 0 && fire_health < 4)
+        {
+            fire_animator.SetInteger("stage", 0);
+            item_area.radius = 0.6f;
+        }
+        else if (fire_health >= 4 && fire_health < 8)
+        {
+            fire_animator.SetInteger("stage", 1);
+            item_area.radius = 0.95f;
+        }
+        else if (fire_health >= 8)
+        {
+            fire_animator.SetInteger("stage", 2);
+            item_area.radius = 1.25f;
+        }
+    }
+
     private void doHealth()
     {
         fire_health = Mathf.Min(fire_health + 1, 10);
         Debug.Log(fire_health);
 
-        if (fire_health >= 0 && fire_health < 4)
-        {
-            fire_animator.SetInteger("stage", 0);
-        }
-        else if (fire_health >= 4 && fire_health < 8)
-        {
-            fire_animator.SetInteger("stage", 1);
-        }
-        else if (fire_health >= 8)
-        {
-            fire_animator.SetInteger("stage", 2);
-        }
+        UpdateHealthEffects();
     }
 
     private void OnTriggerEnter2D(Collider2D col)
